563a4b62e5cf839e6ed442b2bdb856a232e230b5
tonipcv <xppsalvador@gmail.com>
2025-03-08 23:13:25 +0000

    teste2

--- notification-panel/app/page.tsx
+++ notification-panel/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     setResult(null)
 
     try {
-      const response = await fetch('http://localhost:3000/send-notification', {
+      const response = await fetch('https://boop-notify-ios-ft.dpbdp1.easypanel.host/send-notification', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
